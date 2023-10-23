4d80dc5674d20672654fcf1ca65676478fd30baf
Muhamad Nurikbal <muhamadnurikbal15@gmail.com>
2023-10-23 20:03:34 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\MahasiswaController;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// Route::get('/contoh', function () {
+//     return view('ini halaman contoh');
+// });
+
+// Route::get('/abc', function () {
+//     return view('ini halaman abc');
+// });
+Route::get('/', [HomeController::class, 'index']);
+Route::get('/mahasiswa/{npm}', [MahasiswaController::class, 'view']);
